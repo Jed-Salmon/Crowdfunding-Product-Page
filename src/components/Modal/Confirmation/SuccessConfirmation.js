7df46ce584592fc9733b82de49b4f4a6ee61ab50
Jed Salmon <jedsalmon@protonmail.com>
2021-10-20 14:15:52 +0100

    switched fill-rule to cameCase to avoid error log in success modal

--- src/components/Modal/Confirmation/SuccessConfirmation.js
+++ src/components/Modal/Confirmation/SuccessConfirmation.js
@@ -10,7 +10,7 @@ const SuccessConfirmation = () => {
     <div className={classes.successModal}>
       <div className={classes.check}>
         <svg width="64" height="64" xmlns="http://www.w3.org/2000/svg">
-          <g fill="none" fill-rule="evenodd">
+          <g fill="none" fillRule="evenodd">
             <circle fill="#3CB3AB" cx="32" cy="32" r="32" />
             <path stroke="#FFF" strokeWidth="5" d="M20 31.86L28.093 40 44 24" />
           </g>
